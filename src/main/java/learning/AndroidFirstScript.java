f5b27e6d87474c26c4ac3fbef3befab9e37f3ac8
Lokesh Kumar Thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2023-06-04 17:22:54 +0530

    Day 2 code added

---
+++ src/main/java/learning/AndroidFirstScript.java
@@ -0,0 +1,25 @@
+package learning;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class AndroidFirstScript {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("platformName","Android");
+        dc.setCapability("deviceName","sdfdg");
+        dc.setCapability("app",System.getProperty("user.dir")+"/apks/leaforg.apk");
+        AppiumDriver driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"),dc);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.findElement(AppiumBy.className("android.widget.EditText")).sendKeys("rajkumar@testleaf.com");
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Password']")).sendKeys("Leaf@123");
+        driver.findElement(AppiumBy.className("android.widget.Button")).click();
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
